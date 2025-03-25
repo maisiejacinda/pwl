af5b396f8bfe867856835a2751955f6fb092bfa6
maisiejacinda <maisiejacinda04@gmail.com>
2025-03-25 22:05:19 +0700

    Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,24 +1,21 @@
 <?php
 
-namespace App\Controllers;  // Mendeklarasikan namespace controller dalam folder App\Controllers
+namespace App\Controllers;
 
-class Home extends BaseController // Kelas Home merupakan turunan dari BaseController
+class Home extends BaseController 
 {
     public function index()
     {
         return view('welcome_message'); 
-        // Metode ini akan menampilkan halaman utama (welcome_message.php) ketika "/" diakses
     }
 
     public function kategori()
     {
         return view('kategori');  
-        // Metode ini akan menampilkan halaman daftar kategori (kategori.php) saat "/kategori" diakses
     }
 
     public function detailKategori($kategori)
     {
-        // Daftar produk berdasarkan kategori
         $produkList = [
             'Alat Tulis' => ['Buku', 'Pena', 'Pensil', 'Penghapus', 'Spidol'],
             'Pakaian' => ['Kaos', 'Kemeja', 'Jaket', 'Celana', 'Sepatu'],
@@ -27,13 +24,8 @@ class Home extends BaseController // Kelas Home merupakan turunan dari BaseContr
             'Snack' => ['Keripik', 'Coklat', 'Permen', 'Kacang', 'Biskuit']
         ];
 
-        // Menyimpan nama kategori yang dipilih ke dalam array data
         $data['kategori'] = $kategori;
-
-        // Mencari produk berdasarkan kategori yang dipilih, jika tidak ada maka nilai defaultnya array kosong
         $data['produk'] = $produkList[$kategori] ?? [];
-
-        // Mengirimkan data ke tampilan detail_kategori.php
         return view('detail_kategori', $data);
     }
 }
